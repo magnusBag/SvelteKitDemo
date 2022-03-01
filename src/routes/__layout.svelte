b4b204d5a15cdc825a58766828956e0050193fb3
Magnus Baggesen <baggesenmagnus@gmail.com>
2022-03-01 22:19:35 +0100

    final background

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
   import Footer from "../components/footer.svelte";
 </script>
 
-<div class=" bg-black px-4 ">
+<div class="bg-black px-4 ">
   <div class="image">
     <Nav />
     <main>
@@ -16,12 +16,26 @@
 
 <style>
   .image {
-    background: url("https://i.ibb.co/LpfJr5r/Farve.png");
+    background-image: url("https://i.ibb.co/LpfJr5r/Farve.png");
     background-position: 50% 190px;
     background-repeat: no-repeat;
-    background-size: 60vw;
+    background-size: 55vw;
     background-attachment: fixed;
-    backdrop-filter: blur(4px);
+  }
+  @media (max-width: 450px) {
+    .image {
+      background-size: 80vw;
+    }
+  }
+  @media (min-width: 1020px) {
+    .image {
+      background-size: 75vw;
+    }
+  }
+  @media (min-width: 1400px) {
+    .image {
+      background-size: 55vw;
+    }
   }
   main {
     font-family: "Poppins", sans-serif;
