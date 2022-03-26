fbbe74f9f021adfbe52286508a608f955e7024e8
Magnus Baggesen <baggesenmagnus@gmail.com>
2022-03-26 19:37:43 +0100

    whole lotta styling

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,7 @@
 <script>
 </script>
 
-<h2 class="text-4xl text-center my-5 uppercase ">Eyooooooo</h2>
-<p>This is a really cool wesite i made to practise sveltekit</p>
-<iframe
-  title="kantenmap"
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36182.88061459491!2d8.461398805123324!3d55.472602203794416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464b212b863b9f21%3A0xda996a903b7c0df7!2sFinsensgade%201%2C%206700%20Esbjerg!5e0!3m2!1sen!2sdk!4v1645955380398!5m2!1sen!2sdk"
-  width="30%"
-  height="30%"
-  style="border:0;"
-  allowfullscreen=""
-  loading="lazy"
-/>
+<div class="h-[100vh]">
+  <h2 class="text-4xl text-center my-5 uppercase text-white">About</h2>
+  <p class="text-white text-center">TBA.</p>
+</div>
